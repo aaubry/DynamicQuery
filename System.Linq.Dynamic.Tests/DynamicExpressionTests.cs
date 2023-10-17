f69dcc2510fbf364b40cdde0c21a1fb6b49407dd
Antoine Aubry <antoine@aaubry.net>
2023-10-17 11:44:09 +0100

    Support dynamically accessing object and dictionary properties

--- System.Linq.Dynamic.Tests/DynamicExpressionTests.cs
+++ System.Linq.Dynamic.Tests/DynamicExpressionTests.cs
@@ -7,98 +7,98 @@ using DynamicExpression = System.Linq.Dynamic.DynamicExpression;
 
 namespace System.Linq.Dynamic.Tests
 {
-	public class DynamicExpressionTests
-	{
-		[Fact]
-		public void ParseSimpleExpressionWorks()
-		{
-			var expression = @"x.Length == 4";
+    public class DynamicExpressionTests
+    {
+        [Fact]
+        public void ParseSimpleExpressionWorks()
+        {
+            var expression = @"x.Length == 4";
 
-			var expr = (Expression<Func<string, bool>>)DynamicExpression.ParseLambda(new[] { Expression.Parameter(typeof(string), "x") }, typeof(bool), expression);
+            var expr = (Expression<Func<string, bool>>)DynamicExpression.ParseLambda(new[] { Expression.Parameter(typeof(string), "x") }, typeof(bool), expression);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var values = new[] { "bar", "dog", "food", "water" }.AsQueryable();
+            var values = new[] { "bar", "dog", "food", "water" }.AsQueryable();
 
-			var results = values.Where(expr).ToList();
+            var results = values.Where(expr).ToList();
 
-			Assert.Equal(1, results.Count);
-			Assert.Equal("food", results[0]);
-		}
+            Assert.Equal(1, results.Count);
+            Assert.Equal("food", results[0]);
+        }
 
-		[Fact]
-		public void ParseSubQueryExpressionWorks()
-		{
-			var expression = "x.Any(it == 'a')";
+        [Fact]
+        public void ParseSubQueryExpressionWorks()
+        {
+            var expression = "x.Any(it == 'a')";
 
-			var expr = (Expression<Func<IEnumerable<char>, bool>>)DynamicExpression.ParseLambda(new[] { Expression.Parameter(typeof(IEnumerable<char>), "x") }, typeof(bool), expression);
+            var expr = (Expression<Func<IEnumerable<char>, bool>>)DynamicExpression.ParseLambda(new[] { Expression.Parameter(typeof(IEnumerable<char>), "x") }, typeof(bool), expression);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var values = new[] { "bar", "dog", "food", "water" }.AsQueryable();
+            var values = new[] { "bar", "dog", "food", "water" }.AsQueryable();
 
-			var results = values.Where(expr).ToList();
+            var results = values.Where(expr).ToList();
 
-			Assert.Equal(2, results.Count);
-			Assert.Equal("bar", results[0]);
-			Assert.Equal("water", results[1]);
-		}
+            Assert.Equal(2, results.Count);
+            Assert.Equal("bar", results[0]);
+            Assert.Equal("water", results[1]);
+        }
 
-		[Fact]
-		public void AccessEnumInExpressionWorks()
-		{
-			var expression = "it == Int32(MyEnum.Yes)";
+        [Fact]
+        public void AccessEnumInExpressionWorks()
+        {
+            var expression = "it == Int32(MyEnum.Yes)";
 
-			var expr = (Expression<Func<int, bool>>)DynamicExpression.ParseLambda(typeof(int), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
+            var expr = (Expression<Func<int, bool>>)DynamicExpression.ParseLambda(typeof(int), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func((int)MyEnum.Yes));
-			Assert.False(func((int)MyEnum.No));
-		}
+            Assert.True(func((int)MyEnum.Yes));
+            Assert.False(func((int)MyEnum.No));
+        }
 
-		[Fact]
-		public void AccessibleTypesCanBeSpecifiedOnEachCall()
-		{
-			DynamicExpression.ParseLambda(typeof(int), typeof(bool), "it != null");
-			DynamicExpression.ParseLambda(typeof(int), typeof(bool), "it == Int32(MyEnum.Yes)", additionalAllowedTypes: new[] { typeof(MyEnum) });
-		}
+        [Fact]
+        public void AccessibleTypesCanBeSpecifiedOnEachCall()
+        {
+            DynamicExpression.ParseLambda(typeof(int), typeof(bool), "it != null");
+            DynamicExpression.ParseLambda(typeof(int), typeof(bool), "it == Int32(MyEnum.Yes)", additionalAllowedTypes: new[] { typeof(MyEnum) });
+        }
 
-		[Fact]
-		public void EnumWithoutCastIsConvertedToDestinationType()
-		{
-			var expression = "it == MyEnum.Yes";
+        [Fact]
+        public void EnumWithoutCastIsConvertedToDestinationType()
+        {
+            var expression = "it == MyEnum.Yes";
 
-			var expr = (Expression<Func<int, bool>>)DynamicExpression.ParseLambda(typeof(int), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
+            var expr = (Expression<Func<int, bool>>)DynamicExpression.ParseLambda(typeof(int), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func((int)MyEnum.Yes));
-			Assert.False(func((int)MyEnum.No));
-		}
+            Assert.True(func((int)MyEnum.Yes));
+            Assert.False(func((int)MyEnum.No));
+        }
 
-		[Fact]
-		public void EnumWithoutCastIsConvertedFromInt32ToInt64()
-		{
-			var expression = "it == MyEnum.Yes";
+        [Fact]
+        public void EnumWithoutCastIsConvertedFromInt32ToInt64()
+        {
+            var expression = "it == MyEnum.Yes";
 
-			var expr = (Expression<Func<long, bool>>)DynamicExpression.ParseLambda(typeof(long), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
+            var expr = (Expression<Func<long, bool>>)DynamicExpression.ParseLambda(typeof(long), typeof(bool), expression, additionalAllowedTypes: new[] { typeof(MyEnum) });
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func((long)MyEnum.Yes));
-			Assert.False(func((long)MyEnum.No));
-		}
+            Assert.True(func((long)MyEnum.Yes));
+            Assert.False(func((long)MyEnum.No));
+        }
 
         [Fact]
         public void CanParseFirstOrDefaultExpression()
@@ -134,116 +134,170 @@ namespace System.Linq.Dynamic.Tests
             Assert.Null(func(new string[] { }));
         }
 
-		[Fact]
-		public void CanParseNestedLambdasWithOuterVariableReference()
-		{
-			var expression = "resource.Any(allowed.Contains(it_1.Item1))";
+        [Fact]
+        public void CanParseNestedLambdasWithOuterVariableReference()
+        {
+            var expression = "resource.Any(allowed.Contains(it_1.Item1))";
 
-			var parameters = new[]
-			{
-				Expression.Parameter(typeof(Tuple<string>[]), "resource"),
-				Expression.Parameter(typeof(string[]), "allowed"),
-			};
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(Tuple<string>[]), "resource"),
+                Expression.Parameter(typeof(string[]), "allowed"),
+            };
 
-			var expr = (Expression<Func<Tuple<string>[], string[], bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
+            var expr = (Expression<Func<Tuple<string>[], string[], bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func(new[] { Tuple.Create("1"), Tuple.Create("2") }, new[] { "1", "3" }));
-			Assert.False(func(new[] { Tuple.Create("1"), Tuple.Create("2") }, new[] { "3" }));
-		}
+            Assert.True(func(new[] { Tuple.Create("1"), Tuple.Create("2") }, new[] { "1", "3" }));
+            Assert.False(func(new[] { Tuple.Create("1"), Tuple.Create("2") }, new[] { "3" }));
+        }
 
-		[Fact]
-		public void CanParseAs()
-		{
-			var expression = "(resource as System.String).Length";
+        [Fact]
+        public void CanParseAs()
+        {
+            var expression = "(resource as System.String).Length";
 
-			var parameters = new[]
-			{
-				Expression.Parameter(typeof(object), "resource"),
-			};
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(object), "resource"),
+            };
 
-			var expr = (Expression<Func<object, int>>)DynamicExpression.ParseLambda(parameters, typeof(int), expression);
+            var expr = (Expression<Func<object, int>>)DynamicExpression.ParseLambda(parameters, typeof(int), expression);
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.Equal(5, func("hello"));
-		}
+            Assert.Equal(5, func("hello"));
+        }
 
-		[Fact]
-		public void CanParseIs()
-		{
-			var expression = "resource is System.String";
+        [Fact]
+        public void CanParseIs()
+        {
+            var expression = "resource is System.String";
 
-			var parameters = new[]
-			{
-				Expression.Parameter(typeof(object), "resource"),
-			};
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(object), "resource"),
+            };
 
-			var expr = (Expression<Func<object, bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
+            var expr = (Expression<Func<object, bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func("hello"));
-			Assert.False(func(2));
-		}
+            Assert.True(func("hello"));
+            Assert.False(func(2));
+        }
 
-		[Fact]
-		public void CanParseNew()
-		{
-			var expression = "new(resource.Length alias Len)";
+        [Fact]
+        public void CanParseNew()
+        {
+            var expression = "new(resource.Length alias Len)";
 
-			var parameters = new[]
-			{
-				Expression.Parameter(typeof(string), "resource"),
-			};
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(string), "resource"),
+            };
 
-			var expr = (Expression<Func<string, object>>)DynamicExpression.ParseLambda(parameters, typeof(object), expression);
+            var expr = (Expression<Func<string, object>>)DynamicExpression.ParseLambda(parameters, typeof(object), expression);
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
-		}
+            var func = expr.Compile();
+        }
 
-		[Fact]
-		public void CanParseIsUsingBuiltInType()
-		{
-			var expression = "resource is Double";
+        [Fact]
+        public void CanParseIsUsingBuiltInType()
+        {
+            var expression = "resource is Double";
 
-			var parameters = new[]
-			{
-				Expression.Parameter(typeof(object), "resource"),
-			};
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(object), "resource"),
+            };
 
-			var expr = (Expression<Func<object, bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
+            var expr = (Expression<Func<object, bool>>)DynamicExpression.ParseLambda(parameters, typeof(bool), expression);
 
-			Console.WriteLine(expr);
+            Console.WriteLine(expr);
 
-			Assert.NotNull(expr);
+            Assert.NotNull(expr);
 
-			var func = expr.Compile();
+            var func = expr.Compile();
 
-			Assert.True(func(2.2));
-		}
-	}
+            Assert.True(func(2.2));
+        }
 
-	public enum MyEnum
-	{
-		Yes,
-		No,
-	}
+        [Fact]
+        public void CanInvokeDynamicallyOnObject()
+        {
+            var expression = "it.works.well";
+
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(object), "it"),
+            };
+
+            var expr = (Expression<Func<object, object>>)DynamicExpression.ParseLambda(parameters, typeof(object), expression);
+
+            Console.WriteLine(expr);
+
+            Assert.NotNull(expr);
+
+            var func = expr.Compile();
+
+            var obj = new { works = new { well = true, badly = false } };
+            Assert.Equal(true, func(obj));
+        }
+
+        [Fact]
+        public void CanInvokeDynamicallyOnDictionary()
+        {
+            var expression = "it.works.well";
+
+            var parameters = new[]
+            {
+                Expression.Parameter(typeof(Dictionary<string, object>), "it"),
+            };
+
+            var expr = (Expression<Func<Dictionary<string, object>, object>>)DynamicExpression.ParseLambda(parameters, typeof(object), expression);
+
+            Console.WriteLine(expr);
+
+            Assert.NotNull(expr);
+
+            var func = expr.Compile();
+
+            var obj = new Dictionary<string, object>
+            {
+                {
+                    "works",
+                    new Dictionary<string, object>
+                    {
+                        { "well", true },
+                        { "badly", false }
+                    }
+                }
+            };
+            Assert.Equal(true, func(obj));
+        }
+    }
+
+    public enum MyEnum
+    {
+        Yes,
+        No,
+    }
 }
